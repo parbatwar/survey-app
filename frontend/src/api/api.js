d1f62b03005775ee43506c940d6b36ca908bd5f5
Parbat Sunuwar <parbatwar@gmail.com>
2026-08-29 11:26:55 +0545

    chore: configure frontend api environment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,12 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 })
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000",
+// })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token")
